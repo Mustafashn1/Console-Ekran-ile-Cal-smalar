163c821d26c773ff982d72b035ca5a70f69a1f61
Mutafa <mustafashn.1@outlook.com>
2024-06-25 11:59:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Console_Ekranı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kisiler ks = new kisiler();
+            string ads;
+            string meslegi;
+            Console.Write("Ad Soyad ve MesleğiniziGiriniz Lütfen : ");
+            ads = Console.ReadLine();
+            meslegi = Console.ReadLine();
+            ks.kisilistesi(ads, meslegi);
+            Console.Read();
+
+        }
+    }
+}
---
+++ kisiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Ekranı
+{
+    internal class kisiler
+    {
+        public void kisilistesi(string adsoyad, string meslek )
+        {
+            for (int i = 1; i <=1; i++)
+            {
+                Console.WriteLine(adsoyad);
+                Console.WriteLine(meslek);
+
+            }
+        }
+    }
+}
